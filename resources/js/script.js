34512af50605b78979c7330a10f1bec8c3e21995
Lee Giles <hebrews110@users.noreply.github.com>
2017-07-10 08:01:26 -0400

    Add audio through 25.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,101 +1,219 @@
 
-var mcguffey = {};
-var lesson1 = [['a', 'cheer.mp3'], ['cat','chime.mp3'], ['rat','chime.mp3'], ['and','cheer.mp3']];
-var lesson = 0;
-var word = 0;
-var times = 0;
+var lesson = new Array();
+lesson[0] = ["empty"];
+lesson[1] = ['Lesson 1', ['a', './resources/audio/a.mp3'], ['cat','./resources/audio/cat.mp3'], ['rat','./resources/audio/rat.mp3'], ['and','./resources/audio/and.mp3'], './resources/audio/explain1.mp3', './resources/audio/story1.mp3', 'story1'];
+lesson[2] = ['Lesson 2', ['at', './resources/audio/at.mp3'], ['the','./resources/audio/the.mp3'], ['ran','./resources/audio/ran.mp3'], ['has','./resources/audio/has.mp3'], ['Anna','./resources/audio/anna.mp3'], './resources/audio/explain2.mp3', './resources/audio/story2.mp3', 'story2'];
+lesson[3] = ['Lesson 3', ['Dan', './resources/audio/dan.mp3'], ['hat','./resources/audio/hat.mp3'], ['fan','./resources/audio/fan.mp3'], ['can','./resources/audio/can.mp3'], './resources/audio/explain3.mp3', './resources/audio/story3.mp3', 'story3'];
+lesson[4] = ['Lesson 4', ['man', './resources/audio/man.mp3'], ['cap','./resources/audio/cap.mp3'], ['lad','./resources/audio/lad.mp3'], ['sat','./resources/audio/sat.mp3'], './resources/audio/explain4.mp3', './resources/audio/story4.mp3', 'story4'];
+lesson[5] = ['Lesson 5', './resources/audio/story5.mp3', 'story5'];
+lesson[6] = ['Lesson 6', ['dog', './resources/audio/dog.mp3'], ['Spot','./resources/audio/spot.mp3'], ['fat','./resources/audio/fat.mp3'], ["Dan's",'./resources/audio/dans.mp3'], './resources/audio/explain6.mp3', './resources/audio/story6.mp3', 'story6'];
+lesson[7] = ['Lesson 7', ['see', './resources/audio/see.mp3'], ['sees','./resources/audio/sees.mp3'], ['frog','./resources/audio/frog.mp3'], ["on",'./resources/audio/on.mp3'], ["log",'./resources/audio/log.mp3'], './resources/audio/explain7.mp3', './resources/audio/story7.mp3', 'story7'];
+lesson[8] = ['Lesson 8', ['it', './resources/audio/it.mp3'], ['stand','./resources/audio/stand.mp3'], ["Anna's",'./resources/audio/annas.mp3'], ["is",'./resources/audio/is.mp3'], ["lamp",'./resources/audio/lamp.mp3'], ["mat",'./resources/audio/mat.mp3'], './resources/audio/explain8.mp3', './resources/audio/story8.mp3', 'story8'];
+lesson[9] = ['Lesson 9', ['not', './resources/audio/not.mp3'], ['he','./resources/audio/he.mp3'], ["him",'./resources/audio/him.mp3'], ["his",'./resources/audio/his.mp3'], ["catch",'./resources/audio/catch.mp3'], ["Tom",'./resources/audio/tom.mp3'], ["horse",'./resources/audio/horse.mp3'], './resources/audio/explain9.mp3', './resources/audio/story9.mp3', 'story9'];
+lesson[10] = ['Lesson 10', './resources/audio/story10.mp3', 'story10'];
+lesson[11] = ['Lesson 11', ['in', './resources/audio/in.mp3'], ['eggs','./resources/audio/eggs.mp3'], ["hen",'./resources/audio/hen.mp3'], ["she",'./resources/audio/she.mp3'], ["box",'./resources/audio/box.mp3'], ["nest",'./resources/audio/nest.mp3'], ["get",'./resources/audio/get.mp3'], ["this",'./resources/audio/this.mp3'], './resources/audio/explain11.mp3', './resources/audio/story11.mp3', 'story11'];
+lesson[12] = ['Lesson 12', ['old', './resources/audio/old.mp3'], ['run','./resources/audio/run.mp3'], ["fox",'./resources/audio/fox.mp3'], './resources/audio/explain12.mp3', './resources/audio/story12.mp3', 'story12'];
+lesson[13] = ['Lesson 13', ['I', './resources/audio/i.mp3'], ['Emma','./resources/audio/emma.mp3'], ["by",'./resources/audio/by.mp3'], ["ducks",'./resources/audio/ducks.mp3'], ["feed",'./resources/audio/feed.mp3'], ["will",'./resources/audio/will.mp3'], ["them",'./resources/audio/them.mp3'], ["pond",'./resources/audio/pond.mp3'], "./resources/audio/explain13.mp3", './resources/audio/story13.mp3', 'story13'];
+lesson[14] = ['Lesson 14', ['to', './resources/audio/to.mp3'], ['holds','./resources/audio/holds.mp3'], ["blind",'./resources/audio/blind.mp3'], ["kind",'./resources/audio/kind.mp3'], ["Mary",'./resources/audio/mary.mp3'], ["hand",'./resources/audio/hand.mp3'], './resources/audio/explain14.mp3', './resources/audio/story14.mp3', 'story14'];
+lesson[15] = ['Lesson 15', './resources/audio/story15.mp3', 'story15'];
+lesson[16] = ['Lesson 16', ['her', './resources/audio/her.mp3'], ['let','./resources/audio/let.mp3'], ["Sue",'./resources/audio/sue.mp3'], ["doll",'./resources/audio/doll.mp3'], ["dress",'./resources/audio/dress.mp3'], ["new",'./resources/audio/new.mp3'], './resources/audio/explain16.mp3', './resources/audio/story16.mp3', 'story16'];
+lesson[17] = ['Lesson 17', ['rob', './resources/audio/rob.mp3'], ['do','./resources/audio/do.mp3'], ["there",'./resources/audio/there.mp3'], ["five",'./resources/audio/five.mp3'], ["bird",'./resources/audio/bird.mp3'], ["tree",'./resources/audio/tree.mp3'], './resources/audio/explain17.mp3', './resources/audio/story17.mp3', 'story17']
+lesson[18] = ['Lesson 18', ['cage', './resources/audio/cage.mp3'], ['pet','./resources/audio/pet.mp3'], ["sing",'./resources/audio/sing.mp3'], ["lives",'./resources/audio/lives.mp3'], ["loves",'./resources/audio/loves.mp3'], ["so",'./resources/audio/so.mp3'], './resources/audio/explain18.mp3', './resources/audio/story18.mp3', 'story18'];
+lesson[19] = ['Lesson 19', ['are', './resources/audio/are.mp3'], ['you','./resources/audio/you.mp3'], ["boys",'./resources/audio/boys.mp3'], ["of",'./resources/audio/of.mp3'], ["yes",'./resources/audio/yes.mp3'], ["fast",'./resources/audio/fast.mp3'], ["too",'./resources/audio/too.mp3'], ["like",'./resources/audio/like.mp3'], ["play",'./resources/audio/play.mp3'], './resources/audio/explain19.mp3', './resources/audio/story19.mp3', 'story19'];
+lesson[20] = ['Lesson 20', './resources/audio/story20.mp3', 'story20'];
+lesson[21] = ['Lesson 21', ['day', './resources/audio/day.mp3'], ['night','./resources/audio/night.mp3'], ["owl",'./resources/audio/owl.mp3'], ["but",'./resources/audio/but.mp3'], ["well",'./resources/audio/well.mp3'], ["best",'./resources/audio/best.mp3'], ["what",'./resources/audio/what.mp3'], ["big",'./resources/audio/big.mp3'], ["eyes",'./resources/audio/eyes.mp3'], ["an",'./resources/audio/an.mp3'], './resources/audio/explain21.mp3', './resources/audio/story21.mp3', 'story21'];
+lesson[22] = ['Lesson 22', ['cows', './resources/audio/cows.mp3'], ['off','./resources/audio/off.mp3'], ["shade",'./resources/audio/shade.mp3'], ["our",'./resources/audio/our.mp3'], ["grass",'./resources/audio/grass.mp3'], ["hot",'./resources/audio/hot.mp3'], ["they",'./resources/audio/they.mp3'], ["barn",'./resources/audio/barn.mp3'], ["come",'./resources/audio/come.mp3'], './resources/audio/explain22.mp3', './resources/audio/story22.mp3', 'story22'];
+lesson[23] = ['Lesson 23', ['soon', './resources/audio/soon.mp3'], ['set','./resources/audio/set.mp3'], ["neck",'./resources/audio/neck.mp3'], ["sun",'./resources/audio/sun.mp3'], ["bell",'./resources/audio/bell.mp3'], ["way",'./resources/audio/way.mp3'], ["their",'./resources/audio/their.mp3'], ["one",'./resources/audio/one.mp3'], "./resources/audio/explain23.mp3", './resources/audio/story23.mp3', 'story23'];
+lesson[24] = ['Lesson 24', ['brave', './resources/audio/brave.mp3'], ['men','./resources/audio/men.mp3'], ["save",'./resources/audio/save.mp3'], ["ship",'./resources/audio/ship.mp3'], ["rock",'./resources/audio/rock.mp3'], ["boat",'./resources/audio/boat.mp3'], ["drown",'./resources/audio/drown.mp3'], ["if",'./resources/audio/if.mp3'], './resources/audio/explain24.mp3', './resources/audio/story24.mp3', 'story24'];
+lesson[25] = ['Lesson 25', './resources/audio/story25.mp3', 'story25'];
+lesson[26] = ['Lesson ', ['her', './resources/audio/her.mp3'], ['let','./resources/audio/let.mp3'], ["Sue",'./resources/audio/sue.mp3'], ["doll",'./resources/audio/doll.mp3'], ["dress",'./resources/audio/dress.mp3'], ["new",'./resources/audio/new.mp3'], './resources/audio/explain16.mp3', './resources/audio/story16.mp3', 'story16'];
+lesson[27] = ['Lesson ', ['rob', './resources/audio/rob.mp3'], ['do','./resources/audio/do.mp3'], ["there",'./resources/audio/there.mp3'], ["five",'./resources/audio/five.mp3'], ["bird",'./resources/audio/bird.mp3'], ["tree",'./resources/audio/tree.mp3'], './resources/audio/explain17.mp3', './resources/audio/story17.mp3', 'story17']
+lesson[28] = ['Lesson ', ['cage', './resources/audio/cage.mp3'], ['pet','./resources/audio/pet.mp3'], ["sing",'./resources/audio/sing.mp3'], ["lives",'./resources/audio/lives.mp3'], ["loves",'./resources/audio/loves.mp3'], ["so",'./resources/audio/so.mp3'], './resources/audio/explain18.mp3', './resources/audio/story18.mp3', 'story18'];
+lesson[29] = ['Lesson ', ['are', './resources/audio/are.mp3'], ['you','./resources/audio/you.mp3'], ["boys",'./resources/audio/boys.mp3'], ["of",'./resources/audio/of.mp3'], ["yes",'./resources/audio/yes.mp3'], ["fast",'./resources/audio/fast.mp3'], ["too",'./resources/audio/too.mp3'], ["like",'./resources/audio/like.mp3'], ["play",'./resources/audio/play.mp3'], './resources/audio/explain19.mp3', './resources/audio/story19.mp3', 'story19'];
+lesson[30] = ['Lesson ', './resources/audio/story20.mp3', 'story20'];
+
+var lessonNumber = 0;
+var practice = [];
+var index = 0;
+var l = 0;
+var wait = 0;
+var click = 0;
 var $page = $('#page');
 var $yes = $('#yes');
-var $no = $('#no');
-var n = [];
-var practice = [];
-var random = "";
+var $lessonList = $("#lessonList");
+var wordWritten = "";
+var L;
+var explain;
+var story;
+var storyWritten;
+var getStory = 1;
 
+var audio = document.createElement('audio');
 
 $(document).ready(function() {
 
-  $("#start").click(function() {
-      $('.directions').css('display', 'none');
-      again();
-   })
+//Pick a lesson
+  $(".choose").click(function() {
+      $('#readButton').css('display', 'flex');
+      $('#lessonList').css('display', 'none');
+      $(this).css('background-image', 'none');
+      $(this).css('color', '#000');
+      var lessonNumber = $(this).attr("id");
+      var lessonNumber = lessonNumber.slice(6);
+      l = 0; //resets variable
+      wait = 0; //resets variable
+      makePractice(lessonNumber);
+   });
 
-function again() {
-  if (times < 3) {
-    times = times + 1;
-    practiceWords();
-  } else {
-    return; //will load story to read
-  }; //closes else
-}; //closes again
+/*creates the practice array, just the words
+As well as sets up the audio for the list of words at the end
+Contains Yes which is the click function to show the list of words
+at the end. Also has the buttons for reading the story and putting up
+the list of words*/
+function makePractice(lessonNumber) {
+  console.log('makePractice');
+  practice = [];
+  var thisLesson = lesson[lessonNumber];
+  var long = thisLesson.length - 4;
+  for (var i=1; i <= long; i++ ) {
+    practice.push(thisLesson[i]);
+    var L = practice.length;
+  } //closes for
+  index = 0;
 
-function practiceWords() {
-  console.log('practice words');
-  var length = lesson1.length;
-  console.log(length);
-  for (var i=0; i < length; i++) {
-    w = lesson1[word]; //chooses the written word
-    console.log(word);
-    practice.push(w);
-    console.log(w);
-    word = word+1;
-    console.log(word);
+  var explainAudio = thisLesson.length - 3;
+  var explain = thisLesson[explainAudio];
+
+  $yes.unbind().click(function() {
+    if (clickYes === 0) {
+      clickYes = 1; //so they can't double click and play the audio twice
+    $page.css('display', 'none');
+    $('#yes').css('display', 'none');
+    $('#endScreen').css('display', 'flex');
+    for (var i=0; i<L; i++) {
+      $('#endScreen').append('<h2>' + practice[i][0] + '</h2>');
   }; //closes for
-  writeWords($page, practice);
+
+  var audioExplain = new Audio(explain);
+  audioExplain.play();
+  }; //closes if
+  }); //closes yes
+
+//Length zero is for the review days where there are no words. It goes straight
+//to the story
+  if (practice.length === 0) {
+    writeStory($page, thisLesson);
+  } else {
+    writeWords($page, practice, index, L);
+  }; //closes else
+
+//Button to read story
+  $("#readButton").unbind().click(function() {
+    $('#endScreen').empty();
+     writeStory($page, thisLesson);
+  });
+
+//button click that shows list of lessons
+  $("#unorderedList").unbind().click(function() {
+     $page.empty();
+    $('#endScreen').empty();
+    var end = thisLesson.length - 1;
+    var getStory = thisLesson[end];
+      $('#'+ getStory).css('display', 'none');
+      $("#readButton").css('display', 'none');
+     $lessonList.css('display', 'flex');
+  });
+
+}; //closes makePractice
+
+//shows the story (which is in html)
+function writeStory($page, thisLesson) {
+  console.log('write story');
+   $('#readButton').css('display', 'none');
+   var end = thisLesson.length - 1;
+   var getStory = thisLesson[end];
+    $page.empty();
+    $('#'+ getStory).css('display','block');
+
+//Plays the story audio
+    $(".readToMe").unbind().click(function() {
+      var storyAudio = thisLesson.length - 2;
+      var getStoryAudio = thisLesson[storyAudio];
+      var audio = new Audio(getStoryAudio);
+      audio.play();
+    });  //  closes read to me
+
+//Plays applause when you've read the story.
+        $(".readIt").click(function() {
+          var audio = new Audio('applause.mp3');
+          audio.play();
+        }); // closes play cheer
+
+}; // closes writeStory
+
+//Writes the first set of words on the page in order
+function newWord($page, index, L) {
+  console.log('new word');
+      click = 0;
+      console.log('l ' + l);
+  if (l < L-1) {
+      l = l + 1;
+      index = l;
+      writeWords($page, practice, index, L);
+  } else {
+      practiceWords($page, index, L);
+  } //closes else
+}; //closes newWord
+
+//Chooses the random words from the lesson, wait holds off showing checkmark
+//until two times the number of words in the lesson have shown randomly
+function practiceWords($page, index, L) {
+  console.log('practice words');
+  if (wait === 2*L) {
+    clickYes = 0;
+    $('#yes').css('display', 'block');
+  } else {
+    wait = wait + 1;
+  } //closes else
+  var index = Math.floor(Math.random() * L);
+  writeWords($page, practice, index, L);
 }; //closes practiceWords
 
-function writeWords($page, practice) {
-  console.log('writeWords');
-  console.log(practice);
+//Writes the new word or random word on the page
+function writeWords($page, practice, index, L) {
+  console.log('write words');
   $page.empty();
-  var index = Math.floor(Math.random() * practice.length);
-  var random = practice[index][0];
-  $page.html('<span>' + random + '</span>');
-
-  $page.click(function() {
-  var audio = new Audio(practice[index][1]);
-  audio.play();
-    window.setTimeout(function() {
-  writeWords($page, practice);
+  $page.css('display', 'flex');
+  wordWritten = practice[index][0];
+  $page.html('<span>' + wordWritten + '</span>');
+  var wordAudio = practice[index][1];
+  audio.src = wordAudio;
+  //unbind keeps things from happening twice
+  $page.unbind().click(function() {
+    if (click === 0) {
+    $('span').css('text-shadow', '0px 0px white');
+    audio.play();
+    click = 1;
+  window.setTimeout(function() {
+    newWord($page, index, L);
 }, 2000);
-}); // closes page click play word
-
-/*
-$no.click(function() {
-  clickNo();
-}); //end clickNo
-*/
-$yes.click(function(index, practice) {
-     times = times + 1;
-    $('#endScreen').css('display', 'block');
-}); //closes yes
+}; // closes if
+}); //closes page click
 
 }; //closes writeWords
-/*
-function clickNo() {
-  writeWords($page, practice);
-} //closes clickNo
 
+//Write directions and close directions
+$('#directions').click(function() {
+    $('.directions').css({
+        'display': 'block',
+        'z-index': '10'
+    });
+});
 
-function clickYes(index, practice) {
-  console.log(practice);
-  practice.splice(index, 1);
-  console.log(practice);
-  console.log('practice length' + practice.length);
-  if (practice.length >= 0) {
-    writeWords($page, practice);
-}//closes if
- else {
-   times = times + 1;
-  $('#endScreen').css('display', 'block');
-} //closes else
-} //closes clickYes
-*/
-//   $("<div><span>"n"</span></div>").appendTo('#page');
-  //  $card.html('<span>' + $card.data('value') + "</span>");
-//  word = word + 1;
-  //} //closes for loop
- // closes function
-
-//document.addEventListener("load", writeWords($page));
+$('#closeDirections').click(function() {
+    $('.directions').css({
+        'display': 'none'
+    });
+});
 
   }); // closes document load
